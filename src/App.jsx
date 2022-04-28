aa42e804630997e5c3bfd61aae1eae6153ee475d
Baiyrta <asanovabaiyrta2001@gmail.com>
2022-04-28 15:56:38 +0600

    create-button-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
+import { Button } from './components/UI/Button'
 
 function App() {
-   return <div className="App">hello</div>
+   return (
+      <div>
+         <Button>+ Создать группу</Button>
+      </div>
+   )
 }
-
 export default App
---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+export const Button = ({ children }) => {
+   return <WrapperButton>{children}</WrapperButton>
+}
+
+const WrapperButton = styled.button`
+   background-color: #3772ff;
+   height: 40px;
+   padding: 10px 24px 10px 16px;
+   color: white;
+   border-radius: 8px;
+   letter-spacing: 1px;
+   font-size: 16px;
+   border: none;
+   outline: none;
+   &:hover {
+      background-color: #1d60ff;
+   }
+   &:active {
+      background-color: #6190ff;
+   }
+`
