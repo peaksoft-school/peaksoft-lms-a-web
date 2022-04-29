719d00ab2627fc7159abe6ec7be688f03b980f51
Baiyrta <asanovabaiyrta2001@gmail.com>
2022-04-29 12:26:00 +0600

    create pagination-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 // import { Pagination } from '@mui/material'
-import { Pagination } from '@mui/material'
+import { Pagination, PaginationItem } from '@mui/material'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import './App.css'
+import styled from '@emotion/styled'
 
 function App() {
    return (
       <div className="App">
-         <Pagination count={37} />
+         <WrapperPagination
+            count={10}
+            color="primary"
+            renderItem={(item) => (
+               <Wrapper
+                  components={{
+                     previous: ArrowBackIosNewIcon,
+                     next: ArrowForwardIosIcon,
+                  }}
+                  {...item}
+               />
+            )}
+         />
       </div>
    )
 }
+
+const WrapperPagination = styled(Pagination)`
+   .MuiPagination-root {
+      color: red;
+      background-color: red;
+   }
+`
+const Wrapper = styled(PaginationItem)`
+   .MuiPaginationItem-icon {
+      color: #3772ff;
+   }
+`
 
 export default App
--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { Pagination as MuiPagination, PaginationItem } from '@mui/material'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 
-export const Pagination = (props) => {
-   return <Pagination count={10} renderItem={(item) => <PagItem {...item} />} />
+export const Pagination = () => {
+   return (
+      <MuiPagination
+         count={10}
+         renderItem={(item) => (
+            <PaginationItem
+               components={{
+                  previous: <ArrowBackIosNewIcon style={{ color: 'green' }} />,
+                  next: (
+                     <ArrowForwardIosIcon
+                        fontSize="30px"
+                        style={{ fill: 'green' }}
+                     />
+                  ),
+               }}
+               {...item}
+            />
+         )}
+      />
+   )
 }
-const PagItem = styled(PaginationItem)`
-   .MuiPaginationItem-root {
-      color: red;
-   }
-`
