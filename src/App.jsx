caf85922d83e578e2045b441f74aac811186c734
ric1709 <abbaskanov@gmail.com>
2022-04-29 12:59:19 +0600

    add ImagePicker UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { ImagePicker } from './components/UI/ImagePicker'
 
 function App() {
-   return <div className="App">hello</div>
+   return (
+      <div className="App">
+         <ImagePicker />
+      </div>
+   )
 }
 
 export default App
