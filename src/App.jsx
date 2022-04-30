fe85441f2e8aed5688f8a39a3d8f9f81f3ce0cf6
mavliuda19 <97443166+mavliuda19@users.noreply.github.com>
2022-04-30 14:06:06 +0600

    finish creating switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import './App.css'
-import { ToggleSwitch } from './components/UI/ToggleSwitch'
 
 function App() {
-   return (
-      <div className="App">
-         <ToggleSwitch id="switcher1" name="switcher1" />
-         <ToggleSwitch id="switcher2" name="switcher2" />
-      </div>
-   )
+   return <div className="App">hello</div>
 }
 
 export default App
