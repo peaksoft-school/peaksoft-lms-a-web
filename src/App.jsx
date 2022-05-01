f7fad24a30abb2fcd69b1fa5594a59b88e30167d
BekembaevaSel <bekembaeva8@gmail.com>
2022-05-01 12:36:49 +0600

    delete branch feature/select

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import SelectUi from './components/UI/SelectUi'
 
 function App() {
-   return <div className="App">hello</div>
+   return (
+      <div className="App">
+         <SelectUi />
+      </div>
+   )
 }
 
 export default App
