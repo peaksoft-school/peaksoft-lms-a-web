55e0221f2bf4f365695b8e892e4639515d049503
BekembaevaSel <bekembaeva8@gmail.com>
2022-05-03 13:37:20 +0600

    changed styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import './App.css'
-import { useState } from 'react'
-import Datepicker from './components/UI/DatePickerUi'
 
 function App() {
-   const [dateValue, setDateValue] = useState(null)
-   const dateChangeHandler = (newValue) => {
-      setDateValue(newValue)
-   }
-   return (
-      <div className="App">
-         <Datepicker onChange={dateChangeHandler} dateValue={dateValue} />
-      </div>
-   )
+   return <div className="App">hello</div>
 }
 
 export default App
