b9afb751a5d197e0c6f10b33e9ac9c913778be12
Baiyrta <asanovabaiyrta2001@gmail.com>
2022-05-03 16:57:03 +0600

    add dinamyc style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import { Button } from './components/UI/Button'
 function App() {
    return (
       <div className="divka">
-         <Button>+ Создать группу</Button>
+         <Button background="#3772ff">+ Создать группу</Button>
+         <div className="box">
+            <Button background="#C91E1E">Отправить</Button>
+         </div>
+         <div className="box">
+            <Button background="none">Отмена</Button>
+         </div>
       </div>
    )
 }
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -6,17 +6,16 @@ export const Button = (props) => {
 }
 
 const WrapperButton = styled.button`
-   background-color: #3772ff;
+   color: ${({ color }) => color || 'white'};
+   background: ${({ background }) => background || ''};
    padding: 10px 24px 10px 16px;
-   color: white;
    border-radius: 8px;
-   letter-spacing: 1px;
    border: none;
    outline: none;
    font-weight: 400;
    font-size: 14px;
    line-height: 20px;
-   letter-spacing: 0.06em;
+   letter-spacing: 0.07em;
    &:hover {
       background-color: #1d60ff;
    }
