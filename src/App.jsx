3d9743f6df47f31fd90ec2dc89100de7a49f620a
Baiyrta <asanovabaiyrta2001@gmail.com>
2022-05-03 15:02:31 +0600

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,7 @@
-import { PaginationItem } from '@mui/material'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import './App.css'
-import styled from '@emotion/styled'
-import { Pagination } from './components/UI/Pagination'
 
 function App() {
-   return (
-      <div className="App">
-         <WrapperPagination
-            count={37}
-            color="primary"
-            renderItem={(item) => (
-               <Wrapper
-                  components={{
-                     previous: ArrowBackIosNewIcon,
-                     next: ArrowForwardIosIcon,
-                  }}
-                  {...item}
-               />
-            )}
-         />
-      </div>
-   )
+   return <div className="App">hello</div>
 }
-
-const WrapperPagination = styled(Pagination)`
-   .css-1uvvthh-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected {
-      color: blue;
-      background-color: white;
-      text-decoration: underline;
-   }
-   .css-1uvvthh-MuiButtonBase-root-MuiPaginationItem-root {
-      color: #8f8e8e;
-   }
-`
-const Wrapper = styled(PaginationItem)`
-   .MuiPaginationItem-icon {
-      color: #3772ff;
-   }
-`
 
 export default App
--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import { Pagination as MuiPagination } from '@mui/material'
+import styled from '@emotion/styled'
 
 export const Pagination = (props) => {
-   return <MuiPagination {...props} />
+   return <StyledPagination {...props} />
 }
+
+const StyledPagination = styled(MuiPagination)`
+   .MuiButtonBase-root {
+      color: #8f8e8e;
+      :hover {
+         background-color: transparent;
+      }
+   }
+   .Mui-selected {
+      color: #3772ff;
+      text-decoration: underline;
+      background-color: white !important;
+   }
+   .MuiPaginationItem-icon {
+      color: #3772ff;
+      font-size: 30px;
+   }
+`
