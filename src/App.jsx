aa40c928af3d17076ed84741d7bd750658b3b6c0
ric1709 <abbaskanov@gmail.com>
2022-05-04 14:24:30 +0600

    create and fix tabs ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import './App.css'
-import { BasicTabs } from './components/UI/Tabs'
 
-const tabs = [
-   {
-      label: 'Учителя',
-      Component: <div>Hello, I am tab 1</div>,
-   },
-   {
-      label: 'Студенты',
-      Component: <div>Hello, I am tab 2</div>,
-   },
-]
 function App() {
-   return (
-      <div className="App">
-         <BasicTabs tabs={tabs} />
-      </div>
-   )
+   return <div className="App">hello</div>
 }
 
 export default App
