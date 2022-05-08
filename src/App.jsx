5f45eb87e7fbf729a759922e328f5e384f0c87f9
BekembaevaSel <bekembaeva8@gmail.com>
2022-05-08 19:21:52 +0600

    clean app comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import './App.css'
-import { BreadCrumbs } from './components/UI/BreadCrumbs'
 
 function App() {
-   const pathsArray = [
-      {
-         path: 'materialUi',
-         name: 'материал юай',
-      },
-      {
-         path: 'administrator',
-         name: 'администратор',
-      },
-      {
-         path: 'student',
-         name: 'студент',
-      },
-   ]
-   return (
-      <div className="App">
-         <BreadCrumbs pathsArray={pathsArray} />
-      </div>
-   )
+   return <div className="App">hello</div>
 }
 
 export default App
