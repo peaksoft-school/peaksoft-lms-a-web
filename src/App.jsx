e51ce70ae8f4a36ae20fe10bd839d97d9505cb78
Baiyrta <asanovabaiyrta2001@gmail.com>
2022-05-13 17:07:19 +0600

    change style

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-// import { AppRoutes } from './routes/AppRoutes'
-import { useState } from 'react'
-import { Select } from './components/UI/select/Select'
+import { AppRoutes } from './routes/AppRoutes'
 
-const options = [
-   {
-      id: 'Online',
-      title: 'Online',
-   },
-   {
-      id: 'Offline',
-      title: 'Offline',
-   },
-]
 function App() {
-   const [value, setValue] = useState('')
-   const handleChange = (e) => {
-      setValue(e.target.value)
-   }
-   const selectedHandler = (selected) => {
-      console.log(selected)
-   }
-   return (
-      <div style={{ width: '200px', margin: '300px' }}>
-         <Select
-            options={options}
-            placeholder="Формат обучения"
-            type={'Second' && true}
-            value={value}
-            onChange={handleChange}
-            selectedOption={selectedHandler}
-         />
-      </div>
-   )
+   return <AppRoutes />
 }
 
 export default App
