b6b96500b55dfc101469750ff8219692db55643b
Baiyrta <asanovabaiyrta2001@gmail.com>
2022-05-19 21:41:44 +0600

    start

---
+++ src/components/instructor/primerPage/PrimerPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AppTable } from '../../UI/table/AppTable'
+
+export const PrimerPage = () => {
+   const options = [
+      {
+         id: '12',
+      },
+   ]
+   return (
+      <div>
+         <AppTable />
+      </div>
+   )
+}
--- src/routes/InstructorRoutes.jsx
+++ src/routes/InstructorRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { PrimerPage } from '../components/instructor/primerPage/PrimerPage'
 import { ROUTES } from '../utils/constants/general'
 
 export const InstructorRoutes = () => {
@@ -10,10 +11,7 @@ export const InstructorRoutes = () => {
                path="/*"
                element={<Navigate to={ROUTES.INSTRUCTOR_COURSES} />}
             />
-            <Route
-               path={ROUTES.INSTRUCTOR_COURSES}
-               element={<div>course</div>}
-            />
+            <Route path={ROUTES.INSTRUCTOR_COURSES} element={<PrimerPage />} />
          </Routes>
       </div>
    )
