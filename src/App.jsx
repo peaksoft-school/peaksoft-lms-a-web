e22fccec4ce9498fed746191cab5fc56266f1d8a
ric1709 <abbaskanov@gmail.com>
2022-05-25 19:42:55 +0600

    fix problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { LessonCard } from './components/UI/lessonCard/LessonCard'
 import { Notification } from './components/UI/notification/Notification'
 import { AppRoutes } from './routes/AppRoutes'
 
 function App() {
-   const video = {
-      name: 'a',
-   }
    return (
       <>
-         {/* <AppRoutes /> */}
-         <LessonCard video={video} title="Test" />
+         <AppRoutes />
          <Notification />
       </>
    )
