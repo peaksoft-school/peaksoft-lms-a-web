6f924d05e1bc12105c7dd22ee58af6daa521ab71
ric1709 <abbaskanov@gmail.com>
2022-06-07 16:29:19 +0600

    aADD

--- src/store/create-test-slice.jsx
+++ src/store/create-test-slice.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { baseFetch } from '../api/baseFetch'
 import { TEST_KEY } from '../utils/constants/general'
